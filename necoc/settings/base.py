aa7e2730c197349bd683f7fa85f8bdca793fd788
timothyakampa <timothyakampa@gmail.com>
2014-09-16 14:59:57 +0300

    Adds django-mongo-engine. Adds pip requirements.txt

--- necoc/settings/base.py
+++ necoc/settings/base.py
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django_mongodb_engine', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'necocdb',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',

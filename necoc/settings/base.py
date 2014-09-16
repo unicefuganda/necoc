e9455da510aa40c5845c344f66aece30aea0624b
timothyakampa <timothyakampa@gmail.com>
2014-09-16 12:19:51 +0300

    Initial project structure

--- necoc/settings/base.py
+++ necoc/settings/base.py
@@ -135,6 +135,8 @@ LOCAL_APPS = (
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.

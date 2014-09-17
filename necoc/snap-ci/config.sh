00a5c4df95067af65c077b481167e40751d0eb49
timothyakampa <timothyakampa@gmail.com>
2014-09-17 14:20:54 +0300

    fixed mongodb service for snap

--- necoc/snap-ci/config.sh
+++ necoc/snap-ci/config.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-sudo echo "[mongodb]
-name=MongoDB Repository
-baseurl=http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/
-gpgcheck=0
-enabled=1" > /etc/yum.repos.d/mongodb.repo
-
-sudo yum update -y
-sudo yum install -y mongodb-org
-sudo service mongod start
-
+sudo mkdir -p /data/db && sudo service mongod restart
 pip install -r requirements.txt

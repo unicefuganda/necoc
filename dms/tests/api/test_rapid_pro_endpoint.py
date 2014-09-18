1afb2871111bc01c8a81e2c52a771f857dc80cea
timothyakampa <timothyakampa@gmail.com>
2014-09-18 09:29:04 +0300

    refactored rapidpro API tests

--- dms/tests/api/test_rapid_pro_endpoint.py
+++ dms/tests/api/test_rapid_pro_endpoint.py
@@ -5,27 +5,24 @@ from dms.tests.base import NoSQLAPITestCase
 
 class RapidProEndPointTest(NoSQLAPITestCase):
 
+    def setUp(self):
+        date_time = datetime.datetime(2014, 9, 17, 16, 0, 49, 807000)
+        self.message = dict(phone_no="+256775019449", text="There is a fire", received_at=date_time, relayer_id=234,
+                       relayer_phone="+256773434324", sms_id=23243, status="Q", direction="I", event="mo_sms")
+
     def tearDown(self):
         RapidProMessage.drop_collection()
 
     def test_should_create_rapidpro_message(self):
-        date_time = datetime.datetime(2014, 9, 17, 16, 0, 49, 807000)
-        message = dict(phone_no="+256775019449", text="There is a fire", received_at=date_time, relayer_id=234,
-                       relayer_phone="+256773434324", sms_id=23243, status="Q", direction="I", event="mo_sms")
-
-        response = self.client.post('/api/v1/rapid-pro/', data=message)
+        response = self.client.post('/api/v1/rapid-pro/', data=self.message)
         self.assertEqual(201, response.status_code)
 
-        retrieved_message = RapidProMessage.objects(**message)
+        retrieved_message = RapidProMessage.objects(**self.message)
         self.assertEqual(1, retrieved_message.count())
 
     def test_should_get_rapidpro_message(self):
-        date_time = datetime.datetime(2014, 9, 17, 16, 0, 49, 807000)
-        message = dict(phone_no="+256775019449", text="There is a fire", received_at=date_time, relayer_id=234,
-                       relayer_phone="+256773434324", sms_id=23243, status="Q", direction="I", event="mo_sms")
-
-        RapidProMessage(**message).save()
+        RapidProMessage(**self.message).save()
 
         response = self.client.get('/api/v1/rapid-pro/', format='json')
         self.assertEqual(200, response.status_code)
-        self.assertDictContainsSubset(message, response.data[0])
+        self.assertDictContainsSubset(self.message, response.data[0])

7353011a4f8e1dec05a7585e7671175da53ddaac
timothyakampa <timothyakampa@gmail.com>
2014-09-19 18:08:08 +0300

    adds back virtual env to avoid re-installing stuff

--- necoc/snap-ci/client.sh
+++ necoc/snap-ci/client.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+source ../necoc_env/bin/activate
+
 cd ../../dms/client/app && bower install
 cd ../../dms/client && npm install
-sudo npm install -g grunt-cli   
+sudo npm install -g grunt-cli

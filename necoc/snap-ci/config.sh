1c53e7397c795bf937b3f11295bcd0851c364a45
timothyakampa <timothyakampa@gmail.com>
2014-09-19 18:04:46 +0300

    removing virtual env

--- necoc/snap-ci/config.sh
+++ necoc/snap-ci/config.sh
@@ -2,9 +2,5 @@
 
 sudo mkdir -p /data/db && sudo service mongod restart
 
-cd ..
-virtualenv necoc_env
-source necoc_env/bin/activate
-cd -
 pip install -r requirements.txt
 pip install python-coveralls
--- necoc/snap-ci/ut.sh
+++ necoc/snap-ci/ut.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-source ../necoc_env/bin/activate
 echo $COVERALLS_REPO_TOKEN >> .coveralls.yml
 coverage run manage.py test
 coveralls

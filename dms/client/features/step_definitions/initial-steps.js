75112f1c748dbc464dd2afcc12259228e6b18a24
timothyakampa <timothyakampa@gmail.com>
2014-09-28 21:56:49 +0300

    Added a fix failing functional test

--- dms/client/features/step_definitions/initial-steps.js
+++ dms/client/features/step_definitions/initial-steps.js
@@ -1,10 +1,15 @@
 module.exports = function () {
-    var dbUtils =  require("../feature_utils/db-helpers.js")();
+    var dbUtils = require("../feature_utils/db-helpers.js")();
 
     this.World = require("../support/world").World;
 
     this.registerHandler('AfterFeatures', function (event, next) {
         next();
+    });
+
+    this.Before(function (callback) {
+        browser.driver.manage().window().setSize(1280, 1024);
+        callback();
     });
 
     this.After(function (callback) {

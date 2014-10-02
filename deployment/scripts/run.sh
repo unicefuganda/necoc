5f33e6a940b5addb51e5b46591d72117da8dfa98
timothyakampa <timothyakampa@gmail.com>
2014-10-02 11:57:54 +0300

    Updated docker scripts to load api tokens.

--- deployment/scripts/run.sh
+++ deployment/scripts/run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+sed -i "s/##API_TOKEN##/$API_TOKEN/g" /etc/uwsgi/apps-enabled/necoc-uwsgi.ini
 sed -i "s/##SERVER_NAME##/$NGINX_SERVER_NAME/g" /etc/nginx/conf.d/necoc.conf && service supervisor start

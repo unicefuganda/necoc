b6e9c78d34eb2cb43e3e8af421d8bfab7bb0dc86
timothyakampa <timothyakampa@gmail.com>
2014-10-03 09:51:21 +0300

    added celery to deployment and minor text change

--- dms/tests/api/test_bulk_sms_endpoint.py
+++ dms/tests/api/test_bulk_sms_endpoint.py
@@ -3,7 +3,6 @@ import json
 from dms.models import SentMessage
 from dms.tests.base import MongoAPITestCase
 from httmock import all_requests, HTTMock
-import requests
 
 
 class TestLocationEndpoint(MongoAPITestCase):
@@ -13,18 +12,6 @@ class TestLocationEndpoint(MongoAPITestCase):
     def setUp(self):
         phone_numbers = ['256775019449', '2345']
         self.bulk_sms_to_post = dict(text="There is a fire", phone_numbers=phone_numbers)
-
-
-    @all_requests
-    def post_successful(self, url, request):
-        rapid_pro_id = 6839436
-        return {'status_code': 201,
-                'content': {"messages": [rapid_pro_id], "sms": [rapid_pro_id]}
-                }
-
-    @all_requests
-    def post_throws_exception(self, url, request):
-        raise HTTPException('I do not accept your message')
 
     def test_should_post_sent_bulk_messages_and_save_returned_ids(self):
         data = json.dumps(self.bulk_sms_to_post)
@@ -51,3 +38,14 @@ class TestLocationEndpoint(MongoAPITestCase):
         self.assertEqual(1, retrieved_sms.count())
         self.assertIsNone(retrieved_sms[0].rapid_pro_id)
         self.assertEqual("I do not accept your message", retrieved_sms[0].error_message)
+
+    @all_requests
+    def post_successful(self, url, request):
+        rapid_pro_id = 6839436
+        return {'status_code': 201,
+                'content': {"messages": [rapid_pro_id], "sms": [rapid_pro_id]}
+                }
+
+    @all_requests
+    def post_throws_exception(self, url, request):
+        raise HTTPException('I do not accept your message')

6e7820fff6005ff2f4633d2103a00e03a03c8d55
timothyakampa <timothyakampa@gmail.com>
2014-10-17 13:20:27 +0300

    Update deployment config to import data to mongo if environment variable exists

--- deployment/scripts/load_fixtures.sh
+++ deployment/scripts/load_fixtures.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ -z "$APP_ENV" ]; then
+if [ ! -z "$APP_ENV" ]; then
     sleep 10;
     mongoimport --db dms --collection location --type csv --headerline --file /necoc/dms/fixtures/districts.csv;
     exit 0;

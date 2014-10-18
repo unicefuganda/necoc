c616e3d6a38542e50dd873140b4910ebfcf0abda
timothyakampa <timothyakampa@gmail.com>
2014-10-18 23:20:24 +0300

    Fixed issues in deployment script loading data

--- deployment/scripts/run.sh
+++ deployment/scripts/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sed -i "s/##API_TOKEN##/$API_TOKEN/g" /etc/uwsgi/apps-enabled/necoc-uwsgi.ini
 sed -i "s/##API_TOKEN##/$API_TOKEN/g" /etc/supervisor/conf.d/supervisor.conf
+sed -i "s/##LOAD_DATA##/$LOAD_DATA/g" /etc/supervisor/conf.d/supervisor.conf
 sed -i "s/##SERVER_NAME##/$NGINX_SERVER_NAME/g" /etc/nginx/conf.d/necoc.conf && service supervisor start

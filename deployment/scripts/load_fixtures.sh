62dc68543164ec717c75c3f56e9a0f9e21337b6d
timothyakampa <timothyakampa@gmail.com>
2014-10-24 18:28:47 +0300

    Updated load fixtures in deployment script.

--- deployment/scripts/load_fixtures.sh
+++ deployment/scripts/load_fixtures.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [  "$LOAD_DATA" == "True" ]; then
     sleep 10;
-    cd /necoc/dms/fixtures && python manage.py import_location uganda_location.csv;
+    cd /necoc/dms/fixtures && python /necoc/manage.py import_location uganda_location_unique.csv;
     exit 0;
 fi

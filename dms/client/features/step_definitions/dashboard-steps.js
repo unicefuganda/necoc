034137de1721afd057540436c988148643e5ba17
timothyakampa <timothyakampa@gmail.com>
2014-10-30 15:19:01 +0300

    Trying to fix functional tests on snap ci

--- dms/client/features/step_definitions/dashboard-steps.js
+++ dms/client/features/step_definitions/dashboard-steps.js
@@ -4,18 +4,26 @@ module.exports = function () {
     this.World = require("../support/world").World;
 
     this.When(/^I click the messages panel chevron$/, function (next) {
-        var animationLength = 1000;
+        var animationLength = 800;
         dashboardPage.sliderButton.click().then(function () {
             browser.sleep(animationLength).then(next);
         });
     });
 
-    this.Then(/^I should see the messages panel (.*)/, function(stateText, next) {
-        var state = stateText == 'open';
+    this.Then(/^I should see the messages panel open/, function(next) {
         var self = this;
         browser.wait(dashboardPage.sliderButton.isDisplayed).then(function () {
-            self.expect(dashboardPage.messagesTitle.isDisplayed()).to.eventually.be[state.toString()]
+            self.expect(dashboardPage.messagesTitle.isDisplayed()).to.eventually.be.true
                 .and.notify(next);
         });
     });
+
+    this.Then(/^I should see the messages panel closed/, function(next) {
+        var self = this;
+        browser.wait(dashboardPage.sliderButton.isDisplayed).then(function () {
+            self.expect(dashboardPage.messagesTitle.isDisplayed()).to.eventually.be.false
+                .and.notify(next);
+        });
+    });
+
 }

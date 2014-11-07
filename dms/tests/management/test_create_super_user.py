191680d6701958c9193c0a3d93ec50a3a26e144e
andreweskeclarke <andreweskeclarke@gmail.com>
2014-11-07 08:53:11 +0300

    Allowing create_super_user to work when profile exists

--- dms/tests/management/test_create_super_user.py
+++ dms/tests/management/test_create_super_user.py
@@ -32,7 +32,40 @@ class CreateSuperuserTest(MongoTestCase):
         self.assertEqual('N/A', user_profile.phone)
         self.assertEqual('Admin', user_profile.name)
 
+    def test_should_handle_existing_profile(self):
+        UserProfile(phone='N/A', name='Admin', location=self.kampala, email='admin@admin.admin').save()
+        FakeCommand().handle()
+        self.assertEqual(1, User.objects().count())
+        user = User.objects().first()
+        user_profile = UserProfile.objects().first()
+        self.assertEqual('admin', user.username)
+        self.assertTrue(check_password('password', user.password))
+        self.assertNotEqual('password', user.password)
+        self.assertEqual('admin@admin.admin', user_profile.email)
+        self.assertEqual(self.kampala, user_profile.location)
+        self.assertEqual('N/A', user_profile.phone)
+        self.assertEqual('Admin', user_profile.name)
+
     def test_should_create_super_user_from_args(self):
+        FakeCommand().handle('new_admin',
+                             'new_password',
+                             'new_admin@admin.admin',
+                             'NewAdmin',
+                             'Kampala',
+                             '1234567890')
+        self.assertEqual(1, User.objects().count())
+        user = User.objects().first()
+        user_profile = UserProfile.objects().first()
+        self.assertEqual('new_admin', user.username)
+        self.assertTrue(check_password('new_password', user.password))
+        self.assertNotEqual('new_password', user.password)
+        self.assertEqual('new_admin@admin.admin', user_profile.email)
+        self.assertEqual(self.kampala, user_profile.location)
+        self.assertEqual('1234567890', user_profile.phone)
+        self.assertEqual('NewAdmin', user_profile.name)
+
+    def test_should_handle_existing_profile_from_args(self):
+        UserProfile(phone='1234567890', name='NewAdmin', location=self.kampala, email='new_admin@admin.admin').save()
         FakeCommand().handle('new_admin',
                              'new_password',
                              'new_admin@admin.admin',

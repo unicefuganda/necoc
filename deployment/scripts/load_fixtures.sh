acf9faa64d6f1bdedff3d684736859cca92e0dce
timothyakampa <timothyakampa@gmail.com>
2014-11-17 12:11:07 +0300

    Fix to assigning admin user a role in production.

--- deployment/scripts/load_fixtures.sh
+++ deployment/scripts/load_fixtures.sh
@@ -2,7 +2,7 @@
 if [  "$LOAD_DATA" == "True" ]; then
     sleep 10;
     cd /necoc/dms/fixtures && python /necoc/manage.py import_location uganda_location_unique.csv;
-    python /necoc/manage.py create_super_user;
     python /necoc/manage.py create_user_groups;
+    python /necoc/manage.py create_super_user;
     exit 0;
 fi

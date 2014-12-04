73d800410d4e7a19df6571047333fa152e51ee39
NavaL <mnandri@thoughtworks.com>
2014-12-04 15:59:58 +0300

    changed load fixtures scripts

--- deployment/scripts/load_fixtures.sh
+++ deployment/scripts/load_fixtures.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [  "$LOAD_DATA" == "True" ]; then
     sleep 10;
-    cd /necoc/dms/fixtures && python /necoc/manage.py import_location uganda_location_from_devtrac2.csv;
+    cd /necoc/dms/fixtures && python /necoc/manage.py import_location uganda_subcounties_ubos_2014.csv;
     python /necoc/manage.py create_user_groups;
     python /necoc/manage.py create_super_user;
     exit 0;

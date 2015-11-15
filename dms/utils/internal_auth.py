986becf3b9a036682f0bdec27cb3a8e6d0554516
Alfred Assey M <asseym@gmail.com>
2015-11-15 21:55:58 +0300

    toggle auto responses on/off

--- dms/utils/internal_auth.py
+++ dms/utils/internal_auth.py
@@ -8,7 +8,6 @@ __author__ = 'asseym'
 
 class InternalCallAuth(authentication.BaseAuthentication):
     def authenticate(self, request):
-        import pdb;pdb.set_trace()
         query_string = request.META.get('QUERY_STRING')
         query_dict = self.parse_query_string(query_string)
         user = User.objects.order_by('-created_at').first()

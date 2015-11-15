b85a019e680d1d42998eaa852f9e48cb52a2a25c
Alfred Assey M <asseym@gmail.com>
2015-11-15 14:41:42 +0300

    pre_save goes in the view

--- dms/api/response_message_endpoint.py
+++ dms/api/response_message_endpoint.py
@@ -13,13 +13,6 @@ auto_response_text = settings.AUTO_RESPONSE_MESSAGE
 
 class ResponseMessageSerializer(serializers.MongoEngineModelSerializer):
 
-    def pre_save(self, obj):
-        self.request.DATA['text'] = settings.AUTO_RESPONSE_MESSAGE
-        phone_no = self.request.DATA.get('phone', None)
-        if not phone_no is None:
-            self.request.DATA['response_to'] = \
-                RapidProMessage.objects.filter(phone_no=phone_no).order_by('-created_at').first()
-
     class Meta:
         model = ResponseMessage
         exclude = ('created_at', 'log')
@@ -30,6 +23,15 @@ class ResponseMessageListCreateView(ListCreateAPIView):
     serializer_class = ResponseMessageSerializer
     queryset = ResponseMessage.objects()
     permission_classes = [Or(build_permission_class('dms.can_manage_messages'), IsGetRequest)]
+
+    def pre_save(self, obj):
+        self.request.DATA['text'] = settings.AUTO_RESPONSE_MESSAGE
+        phone_no = self.request.DATA.get('phone', None)
+        if not phone_no is None:
+            self.request.DATA['response_to'] = \
+                RapidProMessage.objects.filter(phone_no=phone_no).order_by('-created_at').first()
+        obj.text = settings.AUTO_RESPONSE_MESSAGE
+        obj.response_to = self.request.DATA['response_to']
 
     def post_save(self, obj, created=True):
         send_one_sms.delay(obj)
